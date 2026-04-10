b326a3cb6e980f057b343bb5a80e1de151ac248a
Akash Kushwaha <23kushwahaakash@gmail.com>
2026-04-10 13:21:15 +0530

    Solved name mismatch

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import { Toaster } from "react-hot-toast";
 import ProtectedRoutes from "./services/ProtectedRoutes";
-import DashBoardPage from "./pages/DashboardPage";
+import DashBoardPage from "./pages/DashBoardPage";
 
 function App() {
   return (
